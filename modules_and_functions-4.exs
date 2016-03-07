e6634b72aadd43e2a4f0650c991036337ea74298
Wei Zeng <wei.zeng@jnsolutions.com.au>
2016-03-08 00:31:41 +1100

    fun5, module_fun 1-5

---
+++ modules_and_functions-4.exs
@@ -0,0 +1,9 @@
+defmodule MySum do
+  def sum(0) do
+    0
+  end
+
+  def sum(n) do
+    n + sum(n - 1)
+  end
+end
---
+++ modules_and_functions-5.exs
@@ -0,0 +1,7 @@
+defmodule MyGCD do
+  def gcd(x, 0), do: x
+
+  def gcd(x, y) do
+    gcd(y, rem(x, y))
+  end
+end
